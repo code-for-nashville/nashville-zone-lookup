50ea50d53c3143432dae2560701032c9ef850f33
Jordan Davis <jdcville@gmail.com>
2017-09-02 21:41:56 -0500

    Add catch-all route for browsers for serving VueJS app.

--- lib/parcel_web/router.ex
+++ lib/parcel_web/router.ex
@@ -16,7 +16,7 @@ defmodule ParcelWeb.Router do
   scope "/", ParcelWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*page", PageController, :index
   end
 
   # Other scopes may use custom stacks.
