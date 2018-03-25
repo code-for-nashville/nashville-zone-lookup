d2d5e55b72aafbbcf3619d734710220b8fa92839
Max Shenfield <shenfieldmax@gmail.com>
2018-03-25 07:36:02 -0500

    Make results from API more consistent

--- lib/parcel_web/controllers/land_use_categories_controller.ex
+++ lib/parcel_web/controllers/land_use_categories_controller.ex
@@ -1,9 +1,15 @@
 defmodule ParcelWeb.LandUseCategoriesController do
   use ParcelWeb, :controller
   alias Parcel.Zoning.LandUse
+
   action_fallback(ParcelWeb.FallbackController)
 
   def index(conn, _params) do
-    json(conn, LandUse.categories())
+    index_to_items = Enum.with_index(LandUse.categories())
+    |> Enum.reduce(%{}, fn ({item, index}, acc) ->
+      Map.put(acc, "#{index}", item)
+    end)
+
+    json(conn, index_to_items)
   end
 end
--- test/parcel_web/controllers/land_use_categories_controller_test.exs
+++ test/parcel_web/controllers/land_use_categories_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule ParcelWeb.LandUseCategoriesControllerTest do
+  use ExUnit.Case, async: true
   use ParcelWeb.ConnCase
 
   import ParcelWeb.Router.Helpers
-  alias Parcel.Zoning.LandUse
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -11,6 +11,20 @@ defmodule ParcelWeb.LandUseCategoriesControllerTest do
   test "Returns canned results", %{conn: conn} do
     conn = get(conn, api_land_use_categories_path(conn, :index))
 
-    assert json_response(conn, 200) == LandUse.categories()
+    assert json_response(conn, 200) == %{
+      "0" => "Residential",
+      "1" => "Institutional",
+      "2" => "Educational",
+      "3" => "Office",
+      "4" => "Medical",
+      "5" => "Commercial",
+      "6" => "Communication",
+      "7" => "Industrial",
+      "8" => "Transportation",
+      "9" => "Utility",
+      "10" => "Waste Management",
+      "11" => "Recreation and Entertainment",
+      "12" => "Other",
+    }
   end
 end
