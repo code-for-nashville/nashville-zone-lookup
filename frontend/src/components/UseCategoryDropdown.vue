952311c3deff5bb351bb7ea9f3a7f35fa7369582
Corey Rice <CoreyAR@users.noreply.github.com>
2018-04-04 21:04:53 -0500

    Style dropdown (#60)
    * Style dropdown
    
    * Revisions
    
    * remove unneeded key

--- frontend/src/components/UseCategoryDropdown.vue
+++ frontend/src/components/UseCategoryDropdown.vue
@@ -1,39 +1,52 @@
 <template>
-  <select @change="$emit('selected', $event.target.value)" class="UseCategoryDropdown">
-    <option value="">Everything</option>
-    <option :value="category" v-for="category in categories">
-      {{ category }} Uses
-    </option>
-  </select>
+<b-dropdown :text="displayCategory(category)" class="UseCategoryDropdown" right>
+  <b-dropdown-item-button class="category-item" @click="onCategorySelected('')">Everything</b-dropdown-item-button>
+  <b-dropdown-item-button :key="category" class="category-item" :value="category" v-for="category in categories" @click="onCategorySelected(category)">
+    {{ category }} Uses
+  </b-dropdown-item-button>
+  </b-dropdown>
 </template>
 
 <script>
+import bDropdown from 'bootstrap-vue/es/components/dropdown/dropdown'
+import bDropdownItemButton from 'bootstrap-vue/es/components/dropdown/dropdown-item-button'
 export default {
-  props: ['categories']
+  name: 'category-dropdown',
+  props: ['categories', 'onCategorySelected', 'category'],
+  components: {bDropdown, bDropdownItemButton},
+  methods: {
+    displayCategory (category) {
+      const display = category ? `${category} Uses` : 'Everything'
+      return display
+    }
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
-
 .UseCategoryDropdown {
-    border: none;
-    background-color: rgba(255, 255, 255, 0.19);
-    color: $light-text-color;
-    font-weight: 500;
-    height: 4rem;
-    min-width: 18rex;
-    padding: .25rem 1.5rem;
-    text-align: left;
+  border: none;
+  color: $light-text-color;
+  font-weight: 500;
+  height: 4rem;
+  min-width: 18rex;
 
-    option {
-        color: $secondary-text-color;
-    }
+  .category-item {
+    color: #fff;
+    font-weight: 500;
+    min-width: 18rex;
+    text-align: left;
+  }
+  .category-item:focus {
+    outline:0;
+    background-color: $secondary-blue;
+  }
 }
 
 @media (min-width: 1199px) {
-    .UseCategoryDropdown {
-        height: 100%;
-    }
+  .UseCategoryDropdown {
+    height: 100%;
+  }
 }
-
 </style>
